6738cd13a35f01caa1173afe2360f55145bfe274
PRIYANKA M V <134724191+priyankamv279@users.noreply.github.com>
2024-01-18 14:04:49 +0530

    sql

---
+++ mAadhar sql.sql
@@ -0,0 +1,12 @@
+create database mAadhar;
+use mAadhar;
+show tables;
+select * from user_object;
+select * from new_aadhar_application_object;
+
+
+
+
+
+truncate table user_object;
+truncate table new_aadhar_application_object;
